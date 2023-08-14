8d3afff525a19ce87e205da85017b9351d28a285
pavantalla <61016137+Pavantalla@users.noreply.github.com>
2023-08-14 11:49:38 +0530

    Add files via upload

--- MinMax.java
+++ MinMax.java
@@ -12,13 +12,14 @@ public class MinMax {
 		a = sc.nextInt();
 		System.out.println("Enter 2nd number: ");
 		b = sc.nextInt();
-		if (a > b) {
-			System.out.println("Min = " + b);
-			System.out.println("Max = " + a);
-		}
-		else {
-			System.out.println("Min = " + a);
-			System.out.println("Max = " + b);
-		}
+		/*
+		 * if (a > b) { System.out.println("Min = " + b); System.out.println("Max = " +
+		 * a); } else { System.out.println("Min = " + a); System.out.println("Max = " +
+		 * b); }
+		 */
+		int min = (a>b) ? b : a;
+		int max = (a<b) ? b : a;
+		System.out.println("Min = " + min);
+		System.out.println("Max = " + max);
 	}
 }
